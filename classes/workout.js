b1667e56ff2005cae3dbd1e400a9b9a204c9b4b0
Ram Sarfian <39989110+ramsar7002@users.noreply.github.com>
2021-08-09 14:32:50 +0300

    Update workout.js

--- classes/workout.js
+++ classes/workout.js
@@ -1,9 +1,10 @@
 export class Workout {
   date = new Date();
   id = Date.now();
-  constructor(coords, distance, duration) {
+  constructor(coords, distance, duration, name) {
     this.coords = coords;
     this.distance = distance; //in km;
     this.duration = duration; //in min;
+    this.name = name;
   }
 }
